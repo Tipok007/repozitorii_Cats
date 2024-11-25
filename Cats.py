99df6331ba2c5e92f36959294d0be9a5877d7f7f
Tipok007 <lost.tank.as@gmail.com>
2024-11-25 19:29:08 +0500

    созданокна функия дл получения котиков

--- Cats.py
+++ Cats.py
@@ -6,13 +6,15 @@ from io import BytesIO
 
 def load_image(url):
     try: # Отправляем GET-запрос с использованием
-        requests.get() response = requests.get(url) # Проверяем успешность запроса (код ответа 200)
+        requests.get()
+        response = requests.get(url) # Проверяем успешность запроса (код ответа 200)
         response.raise_for_status() # Читаем байты из ответа в объект
-        BytesIO image_data = BytesIO(response.content) # Открываем изображение с помощью
-PIL img = Image.open(image_data)
-return ImageTk.PhotoImage(img)
-except Exception as e: print(f"Ошибка при загрузке изображения: {e}")
-return None
+        image_data = BytesIO(response.content) # Открываем изображение с помощью BytesIO
+        PIL img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+    print(f"Ошибка при загрузке изображения: {e}")
+    return None
 
 window = Tk()
 window.title("Cats!")
